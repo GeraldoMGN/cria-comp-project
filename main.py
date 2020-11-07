3656ae58389727c358eecdffc1929c4daad4339b
Geraldo Galvao <gmgn@cin.ufpe.br>
2020-11-07 15:02:20 -0300

    Refactor

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from PIL import Image
+import os
 import requests
 from random import randint
 import boto3
 import json
+import tempfile
+
+
+# Flags
+should_remove_background = False  # Usar com moderação
+
+
+def get_filename_no_extension(filename):
+    return filename.split('.')[-2].split('/')[-1]
+
+# Extrai caracteristicas da face detectada
 
 
 def detect_faces(photo):
@@ -15,63 +27,72 @@ def detect_faces(photo):
     return response['FaceDetails']
 
 
-def detect_labels_local_file(photo):
-    client = boto3.client('rekognition')
-
-    with open(photo, 'rb') as image:
-        response = client.detect_labels(Image={'Bytes': image.read()})
-
-    return response['Labels']
+# Extrai a emoção da face com maior certeza
+def predict_label(image_filename):
+    face_details = detect_faces(image_filename)
+    image_emotions = face_details[0]["Emotions"]
+    return image_emotions[0]["Type"]
 
 
-# Arquivo de input
-image_filename = "photo.jpg"
+# Chama a API para retirar o background
+def remove_background(filename):
+    filename = get_filename_no_extension(filename) + '-no-background.png'
+    if (should_remove_background):
+        response = requests.post(
+            'https://api.remove.bg/v1.0/removebg',
+            files={'image_file': open(image_filename, 'rb')},
+            data={'size': 'auto'},
+            headers={'X-Api-Key': 'LAHpJwBK3Ly6GsqLuE2tb5Kh'},
+        )
 
-# API de remoção de
-remove_background = False  # Usar com moderação
-if (remove_background):
-    response = requests.post(
-        'https://api.remove.bg/v1.0/removebg',
-        files={'image_file': open(image_filename, 'rb')},
-        data={'size': 'auto'},
-        headers={'X-Api-Key': 'LAHpJwBK3Ly6GsqLuE2tb5Kh'},
-    )
+        if response.status_code == requests.codes.ok:
+            with open(filename, 'wb') as out:
+                out.write(response.content)
+            image_no_background = response.content
+        else:
+            print("Error:", response.status_code, response.text)
+    return filename
 
-    if response.status_code == requests.codes.ok:
-        with open('photo-no-background.png', 'wb') as out:
-            out.write(response.content)
-        image_no_background = response.content
-    else:
-        print("Error:", response.status_code, response.text)
 
-# Extrai caracteristicas da imagem
-#label_count = detect_labels_local_file(image_filename)
-#print("Labels detected: " + str(label_count))
+# Redimencionado foto baseado na proporção alvo/fonte
+def resize_image(source_filename, target_filename):
+    source = Image.open(source_filename)
+    target = Image.open(target_filename)
+    propResize = min(target.size[0] / source.size[0],
+                     target.size[1] / source.size[1])
+    resized_photo = source.resize((int(source.size[0] * propResize),
+                                   int(source.size[1] * propResize)), Image.ANTIALIAS)
+    filename = get_filename_no_extension(source_filename) + '-resized.png'
+    resized_photo.save(filename, "PNG")
+    return filename
 
-# Extrai caracteristicas da face detectada
-face_details = detect_faces(image_filename)
-#print("Image Details: " + str(face_details))
-image_emotions = face_details[0]["Emotions"]
-predicted_label = image_emotions[0]["Type"]
-print(predicted_label)
-
-# Carregando foto sem o background
-image_no_background = Image.open("photo-no-background.png")
-
-# Seleciona um background aleatório baseado na emoção identificada
-backgroundPath = "./IMG/" + \
-    str(predicted_label) + "/0" + str(randint(1, 1)) + ".jpg"
-
-# Carrega background
-background = Image.open(backgroundPath)
-
-# Redimencionado foto baseado na proporção background/foto
-propResize = min(background.size[0] / image_no_background.size[0],
-                 background.size[1] / image_no_background.size[1])
-image_no_background = image_no_background.resize((int(image_no_background.size[0] * propResize),
-                                                  int(image_no_background.size[1] * propResize)), Image.ANTIALIAS)
 
 # Junção da foto sem o background + background novo
-background.paste(image_no_background, (int(
-    (background.size[0] - image_no_background.size[0]) * 0.25), background.size[1] - image_no_background.size[1]), image_no_background)
-background.save('photo-final.png', "PNG")
+def paste_image(source_filename, target_filename):
+    source = Image.open(source_filename)
+    target = Image.open(target_filename)
+    target.paste(source, (int(
+        (target.size[0] - source.size[0]) * 0.5), target.size[1] - source.size[1]), source)
+    filename = get_filename_no_extension(
+        source_filename) + '-' + get_filename_no_extension(target_filename) + '.png'
+    target.save(filename, "PNG")
+    return filename
+
+
+if __name__ == '__main__':
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        # Arquivo de input
+        image_filename = "photo.jpg"
+
+        image_no_bg_filename = remove_background(image_filename)
+
+        predicted_label = predict_label(image_no_bg_filename)
+
+        # Seleciona um background aleatório baseado na emoção identificada
+        background_filename = "./IMG/" + \
+            str(predicted_label) + "/0" + str(randint(1, 1)) + ".jpg"
+
+        image_no_bg_resized_filename = resize_image(
+            image_no_bg_filename, background_filename)
+
+        paste_image(image_no_bg_resized_filename, background_filename)
